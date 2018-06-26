8396e5d6e42290099329952eeec02ca1cf554994
schrej-zz <joerg_schreiner@yahoo.com>
2018-06-26 15:41:46 +0200

    Update tx_bzdstaffdirectory_functions.php

--- Configuration/TCA/tx_bzdstaffdirectory_functions.php
+++ Configuration/TCA/tx_bzdstaffdirectory_functions.php
@@ -22,4 +22,77 @@ $TCA["tx_bzdstaffdirectory_functions"] = Array (
 		'iconfile' => 'EXT:bzdstaffdirectory/icon_tx_bzdstaffdirectory_functions.gif',
 	),
 );
+
+*
+ * This is the default Table Configuration Array for the functions table.
+ */
+$TCA['tx_bzdstaffdirectory_functions'] = array(
+	'ctrl' => $TCA['tx_bzdstaffdirectory_functions']['ctrl'],
+	'interface' => array(
+		'showRecordFieldList' => 'hidden,title'
+	),
+	'columns' => array(
+		'hidden' => array(
+			'l10n_mode' => $hideNewLocalizations,
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.php:LGL.hidden',
+			'config' => array(
+				'type' => 'check',
+				'default' => '0'
+			)
+		),
+		'title' => array(
+			'l10n_mode' => $l10n_mode,
+			'exclude' => 1,
+			'label' => 'LLL:EXT:bzdstaffdirectory/locallang_db.php:tx_bzdstaffdirectory_functions.title',
+			'config' => array(
+				'type' => 'input',
+				'size' => '30',
+			)
+		),
+        'sys_language_uid' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.language',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'special' => 'languages',
+                'items' => [
+                    [
+                        'LLL:EXT:lang/locallang_general.xlf:LGL.allLanguages',
+                        -1,
+                        'flags-multiple'
+                    ],
+                ],
+                'default' => 0,
+            ]
+        ],
+		'l18n_parent' => array(
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.php:LGL.l18n_parent',
+            'config' => array(
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    ['', 0],
+                ],
+				'foreign_table' => 'tx_bzdstaffdirectory_functions',
+				'foreign_table_where' => 'AND tx_bzdstaffdirectory_functions.uid=###CURRENT_PID### AND tx_bzdstaffdirectory_functions.sys_language_uid IN (-1,0)',
+                'showIconTable' => false
+			)
+		),
+		'l18n_diffsource' => array(
+			'config'=>array(
+				'type'=>'passthrough')
+		)
+	),
+	'types' => array(
+		'0' => array('showitem' => 'hidden;;1;;1-1-1, title')
+	),
+	'palettes' => array(
+		'1' => array('showitem' => '')
+	)
+);
+
 ?>
