628e9d277c08e4fc9ee64415f3a7cf135a6b920c
schrej-zz <joerg_schreiner@yahoo.com>
2018-06-26 15:39:29 +0200

    Update tx_bzdstaffdirectory_locations.php

--- Configuration/TCA/tx_bzdstaffdirectory_locations.php
+++ Configuration/TCA/tx_bzdstaffdirectory_locations.php
@@ -22,4 +22,128 @@ $TCA["tx_bzdstaffdirectory_locations"] = Array (
 		'iconfile' => 'EXT:bzdstaffdirectory/icon_tx_bzdstaffdirectory_locations.gif',
 	),
 );
+
+*
+ * This is the default Table Configuration Array for the locations table.
+ */
+$TCA['tx_bzdstaffdirectory_locations'] = array(
+	'ctrl' => $TCA['tx_bzdstaffdirectory_locations']['ctrl'],
+	'interface' => array(
+		'showRecordFieldList' => 'hidden,title'
+	),
+	'columns' => array(
+		'hidden' => array(
+			'l10n_mode' => $hideNewLocalizations,
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.php:LGL.hidden',
+			'config' => array(
+				'type' => 'check',
+				'default' => '0'
+			)
+		),
+		'title' => array(
+			'l10n_mode' => $l10n_mode,
+			'exclude' => 1,
+			'label' => 'LLL:EXT:bzdstaffdirectory/locallang_db.php:tx_bzdstaffdirectory_locations.title',
+			'config' => array(
+				'type' => 'input',
+				'size' => '30',
+			)
+		),
+		'address' => array(
+			'l10n_mode' => $l10n_mode,
+			'exclude' => 1,
+			'label' => 'LLL:EXT:bzdstaffdirectory/locallang_db.php:tx_bzdstaffdirectory_locations.address',
+			'config' => array(
+				'type' => 'text',
+				'cols' => '30',
+				'rows' => '5'
+			)
+		),
+		'zip' => array(
+			'l10n_mode' => $l10n_mode,
+			'exclude' => 1,
+			'label' => 'LLL:EXT:bzdstaffdirectory/locallang_db.php:tx_bzdstaffdirectory_locations.zip',
+			'config' => array(
+				'type' => 'input',
+				'size' => '30',
+			)
+		),
+		'city' => array(
+			'l10n_mode' => $l10n_mode,
+			'exclude' => 1,
+			'label' => 'LLL:EXT:bzdstaffdirectory/locallang_db.php:tx_bzdstaffdirectory_locations.city',
+			'config' => array(
+				'type' => 'input',
+				'size' => '30',
+			)
+		),
+		'country' => array(
+			'l10n_mode' => $l10n_mode,
+			'exclude' => 1,
+			'label' => 'LLL:EXT:bzdstaffdirectory/locallang_db.php:tx_bzdstaffdirectory_locations.country',
+			'config' => array(
+				'type' => 'input',
+				'size' => '30',
+			)
+		),
+		'infopage' => array(
+			'l10n_mode' => 'exclude',
+			'exclude' => 1,
+			'label' => 'LLL:EXT:bzdstaffdirectory/locallang_db.php:tx_bzdstaffdirectory_locations.infopage',
+			'config' => array(
+				'type' => 'group',
+				'internal_type' => 'db',
+				'allowed' => 'pages',
+				'size' => 1,
+				'minitems' => 0,
+				'maxitems' => 1,
+				'show_thumbs' => 1
+			)
+		),
+        'sys_language_uid' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.language',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'special' => 'languages',
+                'items' => [
+                    [
+                        'LLL:EXT:lang/locallang_general.xlf:LGL.allLanguages',
+                        -1,
+                        'flags-multiple'
+                    ],
+                ],
+                'default' => 0,
+            ]
+        ],
+        'l18n_parent' => array(
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.php:LGL.l18n_parent',
+            'config' => array(
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    ['', 0],
+                ],
+				'foreign_table' => 'tx_bzdstaffdirectory_groups',
+				'foreign_table_where' => 'AND tx_bzdstaffdirectory_groups.uid=###CURRENT_PID### AND tx_bzdstaffdirectory_groups.sys_language_uid IN (-1,0)',
+                'showIconTable' => false
+			)
+		),
+		'l18n_diffsource' => array(
+			'config'=>array(
+				'type'=>'passthrough')
+		)
+	),
+	'types' => array(
+		'0' => array('showitem' => 'hidden;;1;;1-1-1, title, address, zip, city, country, infopage')
+	),
+	'palettes' => array(
+		'1' => array('showitem' => '')
+	)
+);
+
 ?>
