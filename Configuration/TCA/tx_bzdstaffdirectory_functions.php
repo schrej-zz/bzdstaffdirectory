988ca2c30f6aad4f9ec8eb061f3bf32834708d68
schrej-zz <joerg_schreiner@yahoo.com>
2018-06-26 14:44:36 +0200

    Typo3 8 TCA Compatibility
    Move tx_bzdstaffdirectory_functions into a separate file

---
+++ Configuration/TCA/tx_bzdstaffdirectory_functions.php
@@ -0,0 +1,29 @@
+<?php
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_bzdstaffdirectory_functions");
+
+$TCA["tx_bzdstaffdirectory_functions"] = Array (
+	'ctrl' => Array (
+		'title' => 'LLL:EXT:bzdstaffdirectory/locallang_db.php:tx_bzdstaffdirectory_functions',
+		'label' => 'title',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'sortby' => 'sorting',
+		'delete' => 'deleted',
+		'prependAtCopy' => 'LLL:EXT:lang/locallang_general.php:LGL.prependAtCopy',
+		'copyAfterDuplFields' => 'sys_language_uid',
+		'useColumnsForDefaultValues' => 'sys_language_uid',
+   		'transOrigPointerField' => 'l18n_parent',
+		'transOrigDiffSourceField' => 'l18n_diffsource',
+		'languageField' => 'sys_language_uid',
+		'enablecolumns' => array (
+			'disabled' => 'hidden',
+		),
+		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'tca.php',
+        'iconfile' => 'EXT:bzdstaffdirectory/icon_tx_bzdstaffdirectory_functions.gif',
+	),
+	'feInterface' => array (
+		'fe_admin_fieldList' => 'hidden, title',
+	)
+);
+?>
