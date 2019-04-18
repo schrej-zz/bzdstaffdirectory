8ebf5b6159a8a37825983816a611749abf1ae510
Joerg Schreiner <joerg_schreiner@yahoo.com>
2019-04-18 13:25:14 +0200

    Working Typo3 8.x version

---
+++ Configuration/TCA/tx_bzdstaffdirectory_functions.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * This is the default Table Configuration Array for the functions table.
+ */
+return array(
+	'ctrl' => Array (
+		'title' => 'LLL:EXT:bzdstaffdirectory/locallang_db.php:tx_bzdstaffdirectory_functions',
+		'label' => 'title',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'sortby' => 'sorting',
+		'delete' => 'deleted',
+		'prependAtCopy' => 'LLL:EXT:lang/locallang_general.php:LGL.prependAtCopy',
+		'copyAfterDuplFields' => 'sys_language_uid',
+		'useColumnsForDefaultValues' => 'sys_language_uid',
+   		'transOrigPointerField' => 'l18n_parent',
+		'transOrigDiffSourceField' => 'l18n_diffsource',
+		'languageField' => 'sys_language_uid',
+		'enablecolumns' => array (
+			'disabled' => 'hidden',
+		),
+		'iconfile' => 'EXT:bzdstaffdirectory/icon_tx_bzdstaffdirectory_functions.gif',
+	),
+	'interface' => array(
+		'showRecordFieldList' => 'hidden,title'
+	),
+	'columns' => array(
+		'hidden' => array(
+			'l10n_mode' => $hideNewLocalizations,
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.php:LGL.hidden',
+			'config' => array(
+				'type' => 'check',
+				'default' => '0'
+			)
+		),
+		'title' => array(
+			'l10n_mode' => $l10n_mode,
+			'exclude' => 1,
+			'label' => 'LLL:EXT:bzdstaffdirectory/locallang_db.php:tx_bzdstaffdirectory_functions.title',
+			'config' => array(
+				'type' => 'input',
+				'size' => '30',
+			)
+		),
+        'sys_language_uid' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.language',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'special' => 'languages',
+                'items' => [
+                    [
+                        'LLL:EXT:lang/locallang_general.xlf:LGL.allLanguages',
+                        -1,
+                        'flags-multiple'
+                    ],
+                ],
+                'default' => 0,
+            ]
+        ],
+		'l18n_parent' => array(
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.php:LGL.l18n_parent',
+            'config' => array(
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    ['', 0],
+                ],
+				'foreign_table' => 'tx_bzdstaffdirectory_functions',
+				'foreign_table_where' => 'AND tx_bzdstaffdirectory_functions.uid=###CURRENT_PID### AND tx_bzdstaffdirectory_functions.sys_language_uid IN (-1,0)',
+			)
+		),
+		'l18n_diffsource' => array(
+			'config'=>array(
+				'type'=>'passthrough')
+		)
+	),
+	'types' => array(
+		'0' => array('showitem' => 'hidden,--palette--;;1,title')
+	),
+	'palettes' => array(
+		'1' => array('showitem' => '')
+	)
+);
+
+?>
