b6d9ae241175109b773b8ca01fc0c2fc16b47188
Joerg Schreiner <joerg_schreiner@yahoo.com>
2020-05-07 18:08:00 +0200

    Another replacement of $_EXTKEY

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-if (TYPO3_MODE=="BE") $TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_bzdstaffdirectory_pi1_wizicon"] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'pi1/class.tx_bzdstaffdirectory_pi1_wizicon.php';
+if (TYPO3_MODE=="BE") $TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_bzdstaffdirectory_pi1_wizicon"] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath(bzdstaffdirectory).'pi1/class.tx_bzdstaffdirectory_pi1_wizicon.php';
 
 ?>
